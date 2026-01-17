fc71cdc32c628fafedc031f451aefa2ea18c5a1e
Stellar Rounin <stellar.rounin+services@gmail.com>
2026-01-17 14:03:29 -0600

    feat: Finish formatting of event actions

--- main.go
+++ main.go
@@ -6,31 +6,57 @@ import (
 	"net/http"
 )
 
-type User struct {
-	Id       int    `json:id`
-	Name     string `json:name`
-	Username string `json:username`
-	Email    string `json:email`
+type Event struct {
+	Id      string  `json:id`
+	Type    string  `json:type`
+	Repo    Repo    `json:repo`
+	Payload Payload `json:payload`
+}
+
+type Repo struct {
+	Id   int    `json:id`
+	Name string `json:name`
+}
+
+type Payload struct {
+	Action      string `json:action`
+	Description string `json:description`
 }
 
 func main() {
 	client := &http.Client{}
 
-	resp, err := client.Get("https://jsonplaceholder.typicode.com/users")
+	resp, err := client.Get("https://api.github.com/users/folke/events")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	var users []User
-
-	err = json.NewDecoder(resp.Body).Decode(&users)
+	var userEvents []Event
+	err = json.NewDecoder(resp.Body).Decode(&userEvents)
 	if err != nil {
 		fmt.Println("Read error: ", err)
 		return
 	}
 
-	for _, user := range users {
-		fmt.Printf("ID: %d | Name: %s\n", user.Id, user.Name)
+	for _, event := range userEvents {
+		fmt.Print("- ")
+
+		switch event.Type {
+		case "IssuesEvent":
+			fmt.Printf("%s an issue in repo %s/n", event.Payload.Action, event.Repo.Name)
+		case "WatchEvent":
+			fmt.Printf("starred repo %s\n", event.Repo.Name)
+		case "IssueCommentEvent":
+			fmt.Printf("%s comment issue in repo %s\n", event.Payload.Action, event.Repo.Name)
+		case "CreateEvent":
+			fmt.Printf("created repo %s: %s\n", event.Repo.Name, event.Payload.Description)
+		case "PushEvent":
+			fmt.Printf("pushed to repo %s\n", event.Repo.Name)
+		case "PullRequestEvent":
+			fmt.Printf("%s pull request in repo %s\n", event.Payload.Action, event.Repo.Name)
+		default:
+			fmt.Printf("%s action performed in repo %s\n", event.Payload.Action, event.Repo.Name)
+		}
 	}
 }
